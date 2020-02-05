47ee307dc897e397935c284793d1990fad0a445b
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-05 19:29:58 +0000

    Add in method to hash the password before saving to the database

--- backend/models/user.js
+++ backend/models/user.js
@@ -39,4 +39,23 @@ const userSchema = new Schema({
    }
 });
 
+userSchema.pre(`save`, function(next){
+    const user = this;
+    if(!user.isModified || !user.isNew) { // Don't perform any of these operations if the user data hasn't been updated
+        next();
+    }
+    else
+    {
+        bcrypt.hash(user.password, stage.saltingRounds, function(err, hash){
+            if(err){
+                console.log(`Error hashing password for user`, user.username);
+                next(err);
+            } else {
+                user.password = hash;
+                next();
+            }
+        })
+    }
+ });
+
 module.exports = mongoose.model(`User`, userSchema);
