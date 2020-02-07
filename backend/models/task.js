1db4c315b7002272328a59eb202091e2376e0db5
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-07 15:52:16 +0000

    Create task file & comments child schema

---
+++ backend/models/task.js
@@ -0,0 +1,14 @@
+const mongoose = require(`mongoose`);
+
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema ({
+    commentUserID: {
+        type: Number,
+        required: true
+    },
+    commentContent: {
+        type: `String`,
+        required: true
+    }
+});
