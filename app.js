1980bea2ec6ceb767502e828c24eed12897c6331
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-07 16:27:20 +0000

    Set up basic app.js skeleton for running the app

---
+++ app.js
@@ -0,0 +1,34 @@
+// This loads values specified in a .env file to process.env.Key
+require(`dotenv`).config();
+
+// Import modules
+const mongoose = require(`mongoose`);
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const logger = require(`morgan`);
+const passport = require(`passport`);
+
+// Get environment e.g. dev or production
+const environment = process.env.NODE_ENV;
+
+// Setup express
+const app = express();
+const router = express.Router();
+
+// Setup body parser for reading POST requests that contain a body
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+if(environment !== `production`) {
+    app.use(logger(`dev`));
+};
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server now listening at localhost:${process.env.PORT}`);
+    mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).catch(function(res){
+        console.log(`Failed to connect with the database`);
+        console.error(res);
+    });
+});
+
+module.exports.app;
