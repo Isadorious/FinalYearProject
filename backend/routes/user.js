274ada1b269cadd53f665181b2e07179c9db1f41
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-12 23:20:55 +0000

    Create route for getting user via ID

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -11,4 +11,12 @@ router.get(`/`, (req, res) => {
 	});
 });
 
+router.get(`/:id`, (req, res) => {
+	let query = User.findById(req.params.id);
+	query.exec((err, user) => {
+		if(err) res.send(err);
+		res.json(user);
+	});
+});
+
 module.exports = router;
