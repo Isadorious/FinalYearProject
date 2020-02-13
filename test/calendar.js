e935ba849209c3233050801cb0cf79317f6a8b33
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-13 17:39:02 +0000

    Create GET route tests

--- test/calendar.js
+++ test/calendar.js
@@ -16,4 +16,40 @@ describe(`Calendar`, () => {
 			done();
 		});
 	});
+
+	/*
+	*	Test the GET calendar route
+	*/
+	describe(`/GET calendar`, () => {
+		it(`it should get all calendars`, (done) => {
+			chai.request(app)
+				.get(`/api/calendars`)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a(`array`);
+					res.body.length.should.be.eql(0);
+					done();
+				});
+		});
+	});
+
+	/*
+	*	Test the GET/:id calendar route
+	*/
+	describe(`/GET/:id calendar`, () => {
+		it(`it should GET a calendar by the given id`, (done) => {
+			let calendar = new Calendar({ calendarName: `Test calendar`});
+			calendar.save((err, calendar) => {
+				chai.request(app)
+					.get(`/api/calendars/` + calendar.id)
+					.send(calendar)
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('calendarName');
+						done();
+					});
+			});
+		});
+	});
 });
