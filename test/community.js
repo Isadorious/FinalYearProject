20ae68ff03527eefef8eee6d963279a2584748a9
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-13 14:22:54 +0000

    Create test for communities get ID - Also indentation fixes

--- test/community.js
+++ test/community.js
@@ -23,12 +23,33 @@ describe(`Community`, () => {
 	describe(`/GET community`, () => {
 		it(`it should get all communities`, (done) => {
 			chai.request(app)
-			.get(`/api/communities`)
-			.end((err, res) => {
-				res.should.have.status(200);
-				res.body.should.be.a(`array`);
-				res.body.length.should.be.eql(0);
-				done();
+				.get(`/api/communities`)
+				.end((err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a(`array`);
+					res.body.length.should.be.eql(0);
+					done();
+				});
+		});
+	});
+
+	/*
+	*	Test the GET/:id community route
+	*/
+	describe(`/GET/:id community`, () => {
+		it(`it should GET a community by the given id`, (done) => {
+			let community = new Community({ communityName: `Test Community`, ownerID: `1` });
+			community.save((err, community) => {
+				chai.request(app)
+					.get(`/api/communities/` + community.id)
+					.send(community)
+					.end((err, community) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('communityName');
+						res.body.should.have.property('ownerID');
+						done();
+					});
 			});
 		});
 	});
