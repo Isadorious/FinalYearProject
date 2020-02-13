3deec513dad410a6f09c9343b3749a15040d25db
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-13 14:09:05 +0000

    Create community GET route

---
+++ backend/routes/community.js
@@ -0,0 +1,12 @@
+var express = require(`express`);
+var router = express.Router();
+var Community = require(`../models/community`);
+
+router.get(`/`, (req, res) => {
+	let query = Community.find({});
+
+	query.exec((err, communities) => {
+		if(err) res.send(err);
+		res.json(communities);
+	});
+});
