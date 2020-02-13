50539e389c1725ddcf370c88632ea0f21d22e9cf
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-13 12:07:52 +0000

    Set mongoose connection to useCreateIndex to stop deprecation warnings

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(`/api/users`, userRoutes);
 
 app.listen(process.env.PORT, function() {
     console.log(`Server now listening at localhost:${process.env.PORT}`);
-    mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false}).catch(function(res){
+    mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex : true}).catch(function(res){
         console.log(`Failed to connect with the database`);
         console.error(res);
     });
