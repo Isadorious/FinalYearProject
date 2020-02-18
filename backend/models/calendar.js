0fbf7d0a56c8f2240c358f25b0b7376cd76bb0db
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-18 02:14:33 +0000

    Move task related schemas to calendar model +task schema in calendar model file +subtask schema in calendar model file +comment schema in calendar model file -task model fille

--- backend/models/calendar.js
+++ backend/models/calendar.js
@@ -2,6 +2,60 @@ const mongoose = require(`mongoose`);
 
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema ({
+    commentUserID: {
+        type: Number,
+        required: true
+    },
+    commentContent: {
+        type: `String`,
+        required: true
+    }
+});
+
+const subTaskSchema = new Schema ({
+    subTaskName: {
+        type: `String`,
+        required: true,
+        trim: true
+    },
+    subTaskDescription: {
+        type: `String`
+    },
+    subTaskAssignedUsers: {
+        type: [Number]
+    },
+    subTaskDue: {
+        type: Date
+    },
+    subTaskComments: {
+        type: [commentSchema]
+    }
+});
+
+const taskSchema = new Schema ({
+    taskName: {
+        type: `String`,
+        required: true,
+        trim: true
+    },
+    taskDescription: {
+        type: `String`
+    },
+    taskCategory: {
+        type: `String`
+    },
+    taskAssignedUsers: {
+        type: [Number]
+    },
+    taskDue: {
+        type: Date
+    },
+    taskComments: {
+        type: [commentSchema]
+    }
+});
+
 const calendarSchema = new Schema({
 	calendarName: {
 		type: `String`,
