9746bfd3637ba11ada88318be55fc8a40d1c52a3
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-21 14:47:39 +0000

    Create test for getting all calendar tasks

--- test/task.js
+++ test/task.js
@@ -20,16 +20,20 @@ describe(`Task`, () => {
 	/*
 	*	Test the GET route
 	*/
-	describe(`/GET task`, () => {
-		it(`it should get all tasks`, (done) => {
-			chai.request(app)
-			.get(`/api/tasks`)
-			.end((err, res) => {
-				res.should.have.status(200);
-				res.body.should.be.a(`array`);
-				res.body.length.should.be.eql(0);
-				done();
+	describe(`/GET all tasks for one calendar`, () => {
+		it(`it should GET all tasks for the calendar with the given id`, (done) => {
+			let calendar = new Calendar({ calendarName: `Test calendar` });
+			calendar.save((err, calendar) => {
+				chai.request(app)
+					.get(`/api/calendars/` + calendar.id + `/tasks`)
+					.send(calendar)
+					.end((err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('array');
+						res.body.length.should.be(0);
+						done();
+					});
 			});
-		})
+		});
 	});
 });
