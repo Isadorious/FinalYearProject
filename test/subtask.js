530f52b987734a26fd9470b4f95a3608c2c228fa
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-22 15:35:42 +0000

    Create test for GET all route

---
+++ test/subtask.js
@@ -0,0 +1,40 @@
+process.env.NODE_ENV = `test`;
+
+const mongoose = require(`mongoose`);
+const Calendar = require(`../backend/models/calendar`);
+
+const chai = require(`chai`);
+const chaiHttp = require(`chai-http`);
+const app = require(`../app`);
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe(`Subtask`, () => {
+    beforeEach((done) => {
+        Calendar.deleteMany({}, (err) => {
+            done();
+        });
+    });
+
+    /*
+    * Test the GET route
+    */
+   describe(`/GET all subtasks for one task`, () => {
+        it(`it should GET all subtasks for the task with the given id`, (done) => {
+            let calendar = new Calendar({calendarName: `Test Calendar`});
+            calendar.tasks.push({taskName: `Example task`});
+            calendar.save((err, calendar) => {
+                chai.request(app)
+                    .get(`/api/calendars` + calendar.id + `/tasks` +calendar.tasks[0].id + `/subtasks`)
+                    .send(calendar)
+                    .end((err, res) => {
+                        res.should.have.status(200);
+                        res.body.should.be.a(`array`);
+                        res.body.length.should.be.eql(0);
+                        done();
+                    });
+            }); 
+        });
+   });
+});
