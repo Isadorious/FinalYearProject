f0305ab50752e225a2a00fc90457147e63d7ddd9
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-02-26 00:49:08 +0000

    Create passport middlewear for registering

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -3,3 +3,18 @@ const localStrategy = require(`passport-local`).Strategy;
 const userModel = require(`../models/user`);
 const jwtStrategy = require(`passport-jwt`).Strategy;
 const extractJWT = require(`passport-jwt`).ExtractJwt;
+
+// Create passport middlewear to handle user registration
+passport.use(`register`, new localStrategy({
+	usernameField: `username`,
+	passwordField: `password`,
+	session: false
+}, async (username, password, done) => {
+	try {
+		const user = await userModel.create({username, password});
+		return done(null, user);
+	} catch (error) {
+		// Send the error to the next middlewear
+		done(error);
+	}
+}));
