25c766624041631635d1eaf218db0a1b3892dedf
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-03-16 16:57:18 +0000

    Create a basic nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,24 +9,25 @@ import {
 } from 'react-router-dom';
 import Login from "./Components/Login";
 import Register from "./Components/Register";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-          </ul>
-
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Navbar.Brand>GCOrg</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/login">Login</Nav.Link>
+              <Nav.Link href="/register">Register</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
           <Switch>
             <Route path="/login">
               <Login />
