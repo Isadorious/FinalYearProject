c64aa774f250db97d5a408d2c599ebc9e28e93dc
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-03-22 18:12:13 +0000

    Create structure for storing key and display name pairs for custom data store

---
+++ backend/models/customData/customDataPairs.js
@@ -0,0 +1,18 @@
+const mongoose = require(`mongoose`);
+
+const Schema = mongoose.Schema;
+
+const customDataPairsSchema = new Schema({
+	displayName: {
+		type: `String`,
+		required: true,
+		trim: true,
+	},
+	key: {
+		type: `String`,
+		required: true,
+		trim: true,
+	}
+});
+
+module.exports = mongoose.model(`customDataPairs`, customDataPairsSchema);
