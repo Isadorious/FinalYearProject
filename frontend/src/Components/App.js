e96ecc905f0d1b7d1621797641e603fb5638f5a6
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-03-30 15:53:40 +0100

    Turn App into a class

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -12,44 +12,45 @@ import Register from "./Register";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import EditProfile from "./EditProfile";
+import Home from "./Home";
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <div>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Navbar.Brand>GCOrg</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/login">Login</Nav.Link>
-              <Nav.Link href="/register">Register</Nav.Link>
-              <Nav.Link href="/profile/e/">Edit Profile</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-          <Switch>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/register">
-              <Register />
-            </Route>
-            <Route path="/profile/e/:id" component={EditProfile} />
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
-}
+class App extends React.Component {
 
-function Home() {
-  return <h2>Home page</h2>;
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Navbar.Brand>GCOrg</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+                <Nav.Link href="/profile/e/">Edit Profile</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+            <Switch>
+              <Route path="/login">
+                <Login />
+              </Route>
+              <Route path="/register">
+                <Register />
+              </Route>
+              <Route path="/profile/e/:id" component={EditProfile} />
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+
 }
 
 export default App;
