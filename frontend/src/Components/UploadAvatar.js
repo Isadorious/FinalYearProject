d00fbd39605e4c219c20b42c80085d926bd5e88b
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-15 15:00:19 +0100

    Create UploadAvatar component

---
+++ frontend/src/Components/UploadAvatar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Axios from 'axios';
+
+
+class ProfilePictureUploader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alertShown: false,
+            alertMessage: 'Unable to update profile picture',
+            selectedFile: null
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({
+            selectedFile: event.target.files[0],
+        })
+    }
+
+    handleFileUpload(e) {
+        e.preventDefault();
+        const data = new FormData();
+        data.append('file', this.state.selectedFile);
+
+        let accessString = localStorage.getItem(`JWT`);
+		if (accessString === null) {
+			this.setState({
+				error: true,
+			});
+		}
+
+		let uID = localStorage.getItem(`UserID`);
+		if (uID === null) {
+			this.setState({
+				error: true,
+			});
+		}
+
+        Axios.post(`http://localhost:9000/uploadProfilePicture/${uID}`, {
+            headers: {Authorization: `JWT ${accessString}`},
+            data,
+        })
+        .then(response => {
+            if(response.data.message === `Profile Picture uploaded!`) {
+                alert(`Profile picture updated!`);
+            } else {
+                this.setState({ alertShown: true});
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Form id="registerLoginForm">
+                <Alert variant="danger" show={this.state.alertShown}>
+                    Error: {this.state.alertMessage}
+                </Alert>
+                <Form.Group>
+                    <Form.File id="avatar" label="Profile Picture" custom onChange={this.handleInputChange} />
+                </Form.Group>
+                <Button variant="secondary" type="submit" onClick={this.handleFileUpload}>Upload profile picture</Button>
+            </Form>
+        )
+    }
+}
+
+export default ProfilePictureUploader;
