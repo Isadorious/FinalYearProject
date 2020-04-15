c2af82fc089a52e8a248f34c2314a3929a8c82c3
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-15 15:00:40 +0100

    Add UploadAvatar component to edit profile

--- frontend/src/Components/EditProfile.js
+++ frontend/src/Components/EditProfile.js
@@ -7,6 +7,7 @@ import Col from "react-bootstrap/Col";
 import Alert from "react-bootstrap/Alert";
 import Axios from 'axios';
 import Loading from './Loading';
+import UploadAvatar from './UploadAvatar';
 
 class ProfileForm extends React.Component {
 	constructor(props) {
@@ -178,6 +179,11 @@ class ProfileForm extends React.Component {
 
 									<Button variant="secondary" type="submit" onClick={this.handleSave}>Save Changes</Button>
 								</Form>
+							</Col>
+						</Row>
+						<Row>
+							<Col>
+								<UploadAvatar />
 							</Col>
 						</Row>
 					</Container>
