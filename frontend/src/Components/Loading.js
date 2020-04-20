47c8bec4da4a40e4d93c17fab5191c257a60c8a6
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-20 14:15:34 +0100

    Centre loading component horizontally

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class Loading extends React.Component {
 	render() {
 		return (
-			<Spinner animation="border" role="status">
-			</Spinner>
+			<Container>
+				<Row>
+					<Col></Col>
+					<Col>
+						<Spinner animation="border" role="status" className={"justifyContentCentre"} />
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
 		)
 	}
 }
