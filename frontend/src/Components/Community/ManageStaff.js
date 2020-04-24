c51e205a549398810f0464d3ccaa449042567d8b
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-24 15:33:18 +0100

    Create form and logic to handle adding a staff member

--- frontend/src/Components/Community/ManageStaff.js
+++ frontend/src/Components/Community/ManageStaff.js
@@ -13,11 +13,15 @@ class ManageStaffForm extends React.Component {
 			errorMessage: 'Unable to load staff data',
 			errorStatus: 500,
 			loading: true,
-			moderators: [],
-			admins: [],
+			communityStaffID: [],
+			communityAdminsID: [],
 			users: [],
 			usernameToFind: '',
 		}
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handlePromote = this.handlePromote.bind(this);
+
 	}
 
 	componentDidMount() {
@@ -64,6 +68,80 @@ class ManageStaffForm extends React.Component {
 					loading: false,
 				});
 			})
+
+			await Axios
+			.get('http://localhost:9000/api/communities/' + this.props.id, {
+				headers: { Authorization: `JWT ${accessString}` }
+			}).then(response => {
+				let data = response.data;
+
+				this.setState({
+					communityAdminsID: data.communityAdminsID,
+					communityStaffID: data.communityStaffID,
+					loading: false,
+				});
+			}).catch(error => {
+				this.setState({
+					error: true,
+					errorMessage: `Unable to retrieve community data`,
+					loading: false,
+				});
+			})
+	}
+
+	handleInputChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        
+        this.setState({
+            [name] : value
+        });
+	}
+	
+	handlePromote(e) {
+		e.preventDefault();
+
+		const user = this.state.users.find(u => u.username === this.state.usernameToFind);
+
+		if(user !== undefined)
+		{
+			alert('Unable to find user');
+			return;
+		} else {
+			let communityStaff = this.state.communityStaffID;
+			communityStaff.push(user._id);
+			this.setState({communityStaffID: communityStaff});
+
+			await Axios
+			.put('http://localhost:9000/api/communities/' + this.props.id, {
+				headers: { Authorization: `JWT ${accessString}` },
+				communityStaffID: this.state.communityStaffID,
+			})
+			.then(response => {
+				if (response.data.message === 'Community updated!') {
+					alert(`Community updated!`);
+				} else {
+					alert(response.data);
+				}
+			})
+		}
+	}
+
+	render() {
+		if (this.state.loading === true) {
+			return(<Loading />)
+		} else if (this.state.error === true) {
+			return (<Error statusCode={this.state.errorStatusCode} message={this.state.errorMessage}/>)
+		} else {
+			<Form id="addStaff" className={"modalForm"}>
+				<Form.Group>
+					<Form.Label>User to promote:</Form.Label>
+					<Form.Control name="usernameToFind" type="text" placeholder="User to promote" value={this.state.usernameToFind} onChange={this.handleInputChange} />
+				</Form.Group>
+				<Button variant="secondary" type="submit" onClick={this.handlePromote}>Find and promote user</Button>
+			</Form>
+		}
 	}
 }
 
