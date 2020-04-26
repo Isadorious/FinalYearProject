c50b370e9f4d6caf0670b25709110945eb3c79b3
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-26 14:43:00 +0100

    Add a util function to check that a user is at least an admin

--- backend/Utils/community.js
+++ backend/Utils/community.js
@@ -8,7 +8,36 @@ export async function isOwner(communityID, userID) {
 			let result = {status: 404, message: `Unable to find community`, permission: false, }
 			return result;
 		} else if(userID == community.ownerID) {
-			let result = {permission: true,}
-		}
+            let result = {permission: true,}
+            return result;
+		} else {
+            let result = {permission: false,}
+            return result;
+        }
 	})
+}
+
+export async function isAdmin(communityID, userID) {
+
+    let result = isOwner(communityID, userID);
+
+    if(result.permission === true)
+    {
+        return result;
+    }
+
+	Community.findById({ _id: req.params.id }, (err, community) => {
+	
+		if(community === null)
+		{
+			let result = {status: 404, message: `Unable to find community`}
+			return result;
+		} else if(community.communityAdminsID.includes(user._id) === true) {
+            let result = {permission: true,}
+            return result;
+		} else {
+            let result = {permission: false,}
+            return result;
+        }
+	});
 }
