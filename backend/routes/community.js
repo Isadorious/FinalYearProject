f7a329c82effcf348cb750197f96b8ab5eaa1ba2
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-26 11:49:36 +0100

    Fix indents

--- backend/routes/community.js
+++ backend/routes/community.js
@@ -27,11 +27,11 @@ router.get(`/:id`, (req, res) => {
 });
 
 router.post(`/`, (req, res) => {
-	passport.authenticate(`jwt`, {session: false}, (err, user, info) => {
-		if(err) {
+	passport.authenticate(`jwt`, { session: false }, (err, user, info) => {
+		if (err) {
 			res.send(err);
-		} else if(info != undefined) {
-			res.json({message: info.message});
+		} else if (info != undefined) {
+			res.json({ message: info.message });
 		} else {
 			// Create new community from the data in the request body
 			const community = new Community(req.body);
@@ -49,47 +49,45 @@ router.post(`/`, (req, res) => {
 				user.save();
 			})
 
-			
+
 		}
 	})(req, res);
 });
 
 router.put(`/:id`, (req, res) => {
-	passport.authenticate(`jwt`, {session: false}, (err, user, info) => {
-		if(err) {
+	passport.authenticate(`jwt`, { session: false }, (err, user, info) => {
+		if (err) {
 			res.send(err);
-		} else if(info != undefined) {
-			res.json({message: info.message});
+		} else if (info != undefined) {
+			res.json({ message: info.message });
 		} else {
-			Community.findById({_id: req.params.id}, (err, community) => {
-				if(err) {
+			Community.findById({ _id: req.params.id }, (err, community) => {
+				if (err) {
 					res.send(err);
 					return;
 				}
 
-				if(community === null)
-				{
-					res.status(404).json({message: `Unable to find community`});
+				if (community === null) {
+					res.status(404).json({ message: `Unable to find community` });
 					return;
 				}
 				// Check if user is owner or admin to allow them to modify the community
 				let hasPerm = false;
-				if(user._id == community.ownerID) {
+				if (user._id == community.ownerID) {
 					hasPerm = true;
-				} else if(community.communityAdminsID.includes(user._id) === true) {
+				} else if (community.communityAdminsID.includes(user._id) === true) {
 					hasPerm = true;
 				}
 
-				if(hasPerm === true)
-				{
+				if (hasPerm === true) {
 					Object.assign(community, req.body).save((err, community) => {
-						if(err) {
+						if (err) {
 							res.send(err);
 						}
-						res.json({message: `Community updated!`, community});
+						res.json({ message: `Community updated!`, community });
 					});
 				} else {
-					res.status(401).json({message: `Not authorized to edit this community`});
+					res.status(401).json({ message: `Not authorized to edit this community` });
 				}
 			});
 		}
