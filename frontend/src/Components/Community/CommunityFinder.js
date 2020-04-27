e0664a5ab1543140e3c74a879c058704ff852215
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-27 23:41:15 +0100

    Fix missing import

--- frontend/src/Components/Community/CommunityFinder.js
+++ frontend/src/Components/Community/CommunityFinder.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import CommunityCard from './CommunityCard';
 
 class CommunityFinder extends React.Component {
 	constructor(props) {
