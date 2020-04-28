e3aadaa2137c3477a254c2ea3b52d0377c688f91
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-28 17:44:32 +0100

    Convert Community Utils to promises

--- backend/Utils/community.js
+++ backend/Utils/community.js
@@ -1,65 +1,72 @@
 const Community = require(`../models/community`);
 
-async function isOwner(communityID, userID) {
-    Community.findById(communityID, (err, community) => {
+function isOwner(communityID, userID) {
+    return new Promise((resolve, reject) => {
+        Community.findById(communityID, (err, community) => {
+    
+            if (community === null) {
+                let result = { status: 404, message: `Unable to find community`, permission: false, }
+                resolve(result);
+            } else if (userID == community.ownerID) {
+                let result = { permission: true, }
+                resolve(result);
+            } else {
+                let result = { permission: false, }
+                resolve(result);
+            }
+        });
+    });
+}
 
-        if (community === null) {
-            let result = { status: 404, message: `Unable to find community`, permission: false, }
-            return result;
-        } else if (userID == community.ownerID) {
-            let result = { permission: true, }
-            return result;
-        } else {
-            let result = { permission: false, }
-            return result;
-        }
+function isAdmin(communityID, userID) {
+    return new Promise((resolve, reject) => {
+        isOwnerP(communityID, userID)
+        .then((result) => {
+            if(result.permission === true) {
+                resolve(result);
+            } else {
+                Community.findById(communityID, (err, community) => {
+                    if (community === null) {
+                        let result = { status: 404, message: `Unable to find community` }
+                        resolve(result);
+                    } else if (community.communityAdminsID.includes(userID) === true) {
+                        let result = { permission: true, }
+                        resolve(result);
+                    } else {
+                        let result = { permission: false, }
+                        resolve(result);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function isStaff(communityID, userID) {
+    return new Promise((resolve, reject) => {
+        isAdminP(communityID, userID)
+        .then((result) => {
+            if(result.permission === true) {
+                resolve(result);
+            } else {
+                Community.findById(communityID, (err, community) => {
+                    if (community === null) {
+                        let result = { status: 404, message: `Unable to find community` }
+                        return result;
+                    } else if (community.communityStaffID.includes(userID) === true) {
+                        let result = { permission: true, }
+                        return result;
+                    } else {
+                        let result = { permission: false, }
+                        return result;
+                    }
+                });
+            }
+        });
     })
 }
 
-async function isAdmin(communityID, userID) {
 
-    let result = isOwner(communityID, userID);
-
-    if (result.permission === true) {
-        return result;
-    }
-
-    Community.findById(communityID, (err, community) => {
-
-        if (community === null) {
-            let result = { status: 404, message: `Unable to find community` }
-            return result;
-        } else if (community.communityAdminsID.includes(user._id) === true) {
-            let result = { permission: true, }
-            return result;
-        } else {
-            let result = { permission: false, }
-            return result;
-        }
-    });
-}
-
-async function isStaff(communityID, userID) {
-    let result = isAdmin(communityID, userID);
-
-    if (result.permission === true) {
-        return result;
-    }
-
-    Community.findById(communityID, (err, community) => {
-
-        if (community === null) {
-            let result = { status: 404, message: `Unable to find community` }
-            return result;
-        } else if (community.communityStaffID.includes(user._id) === true) {
-            let result = { permission: true, }
-            return result;
-        } else {
-            let result = { permission: false, }
-            return result;
-        }
-    });
-}
 
 exports.isOwner = isOwner;
 exports.isStaff = isStaff;
