dd53c234effbdfe3edb7bf909233b422c2258df6
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-29 02:25:03 +0100

    Install HolderJS for placeholder images

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'holderjs';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
