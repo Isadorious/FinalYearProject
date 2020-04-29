68b4df35fe7ce40148538f1420e1f7330f148e38
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-04-29 02:14:38 +0100

    Make CommunityCard invisible if a 404 error is returned from the API

--- frontend/src/Components/Community/CommunityCard.js
+++ frontend/src/Components/Community/CommunityCard.js
@@ -3,7 +3,7 @@ import Card from 'react-bootstrap/Card';
 import Axios from 'axios';
 import Loading from '../Utils/Loading';
 import Error from '../Utils/Error';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class CommunityCard extends React.Component {
 	constructor(props) {
@@ -17,6 +17,7 @@ class CommunityCard extends React.Component {
 			error: false,
 			errorMessage: 'Unable to load community',
 			errorStatusCode: 500,
+			visible: true,
 		}
 	}
 
@@ -49,10 +50,9 @@ class CommunityCard extends React.Component {
 			return;
 		}
 
-		if(this.props.communityID === undefined)
-		{
+		if (this.props.communityID === undefined) {
 			this.setState({
-				error: true, 
+				error: true,
 				errorMessage: 'Community ID not supplied',
 				loading: false
 			});
@@ -60,47 +60,53 @@ class CommunityCard extends React.Component {
 		}
 
 		await Axios
-		.get('http://localhost:9000/api/communities/' + this.props.communityID, {
-			headers: {Authorization: `JWT ${accessString}`}
-		}).then(response => {
-			let data = response.data;
+			.get('http://localhost:9000/api/communities/' + this.props.communityID, {
+				headers: { Authorization: `JWT ${accessString}` }
+			}).then(response => {
+				let data = response.data;
 
-			if(data.description.length > 100)
-			{
-				var res = data.description.substring(0, 96);
-				res += "..."
-				data.description = res;
-			}
+				if (data.description.length > 100) {
+					var res = data.description.substring(0, 96);
+					res += "..."
+					data.description = res;
+				}
 
-			this.setState({
-				name: data.communityName,
-				description: data.description,
-				logo: data.logo,
-				banner: data.banner,
-				loading: false,
-			});
-		}).catch(error => {
-			this.setState({
-				error: true,
-				errorMessage: `Unable to retrieve community data`,
-				loading: false,
-			});
-		})
+				this.setState({
+					name: data.communityName,
+					description: data.description,
+					logo: data.logo,
+					banner: data.banner,
+					loading: false,
+				});
+			}).catch(error => {
+				if (error.response.status == 404) {
+					this.setState({
+						visible: false,
+					})
+				}
+				this.setState({
+					error: true,
+					errorMessage: `Unable to retrieve community data`,
+					loading: false,
+				});
+			})
 	}
 
 	render() {
-		if(this.state.loading === true) {
+		if (this.state.visible === false) {
+			return (<></>)
+		} else if (this.state.loading === true) {
 			return (
 				<Card id="loading" className={"infoCard"}>
 					<Loading />
 				</Card>
 			)
-		} else if(this.state.error == true) {
-			return (			
+		} else if (this.state.error == true) {
+			return (
 				<Card id="error" className={"infoCard"}>
-					<Error message={this.state.errorMessage}/>
+					<Error message={this.state.errorMessage} />
 				</Card>
-				)
+			)
 		} else {
 			return (
 				<Card id={this.props.communityID} className={"infoCard"}>
