a3281905d04b1b449ca5ace85c8ff2a1b442a475
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-05-05 15:04:58 +0100

    Import css for super select

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,5 +4,6 @@ import App from './Components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'holderjs';
 import './index.css';
+import 'react-super-select/lib/react-super-select.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
