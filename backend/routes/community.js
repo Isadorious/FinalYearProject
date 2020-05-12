62d8f5645f8caa217797c6ac31b361165709340f
Isadorious <47084294+Isadorious@users.noreply.github.com>
2020-05-12 11:19:08 +0100

    Add route to get one structure

--- backend/routes/community.js
+++ backend/routes/community.js
@@ -308,4 +308,33 @@ router.get(`/:id/structures`, (req, res) => {
 	})(req, res);
 });
 
+router.get(`/:communityID/structures/:structureID`, (req, res) => {
+	passport.authenticate(`jwt`, {session: false}, (err, user, info) => {
+		if(err) {
+			res.send(err);
+		} else if(info !== undefined) {
+			res.json({message: info.message});
+		} else {
+			const query = Community.findById(req.params.communityID);
+			query.exec((err, community) => {
+				if(err) {
+					res.send(err);
+					return;
+				}
+				if(community === null) {
+					res.status(404).json({message: `No community found`});
+				} else {
+					isStaff(community._id, user._id)
+						.then((result) => {
+							let dataStore = community.dataStores.id(req.params.structureID);
+							res.json(dataStore);
+						}).catch((result) => {
+							res.status(result.status).json({message: result.message});
+						})
+				}
+			});
+		}
+	})(req, res);
+});
+
 module.exports = router;
